aeade068a34b529242221a3e6afd08773df44bf7
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2023-05-08 10:06:16 -0700

    Basic Movement Given to Player

---
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+    
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        
+    }
+}
