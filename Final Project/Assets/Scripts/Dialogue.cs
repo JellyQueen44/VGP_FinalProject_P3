52a95f261aa84056e2a0aa90ae6dffbc59db79c0
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2023-05-10 10:44:16 -0700

    Basic Dialogue System Created

--- Final Project/Assets/Scripts/Dialogue.cs
+++ Final Project/Assets/Scripts/Dialogue.cs
@@ -8,33 +8,65 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
+
     private int index;
 
     // Start is called before the first frame update
     void Start()
     {
-        textCompoent text = string.Empty;
+        //Getting components and initiates the dialogue start
+        textComponent.text = string.Empty;
         StartDialogue();
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+                
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
 
     void StartDialogue()
     {
+        //Starts the dialogue
         index = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
-        foreach (char c in lines[index] ToCharArray())
+        //Handles the speed that each letter text appears
+        foreach (char c in lines[index].ToCharArray())
         {
-            textComponent text =+ c;
+            textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
         }
     }
 }
